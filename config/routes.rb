a979e2bfe4569649b539dffad2a7853da08033ca
Mshu72 <m.shu.720@gmail.com>
2024-07-30 23:31:11 +0900

    sns認証設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
 
   authenticated :user do
     root to: 'patients#index', as: :authenticated_root
